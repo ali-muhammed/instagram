243b59ab2a4bebbef18ee6859391e5efe7715313
Mukhammedali Rakhimjan <muhamad030523@gmail.com>
2018-12-12 15:56:15 +0600

    I created a personal profile page.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy] 
 
 	def new
-		@post = post.new
+		@post = Post.new
 	end
 
 	def create
